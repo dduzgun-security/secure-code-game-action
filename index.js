675f79225c41e6b281d00379a52ec2364be841fe
dduzgun-security <deniz.duzgun@swiftconnect.io>
2023-04-03 16:29:17 -0400

    Remove extra information

--- index.js
+++ index.js
@@ -53,7 +53,6 @@ try {
   
   
   const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
